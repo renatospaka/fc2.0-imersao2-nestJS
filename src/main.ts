b235b334fa282bfbc0221979027d342f1d82d100
Renato Spakauskas <renatospaka@gmail.com>
2021-01-30 13:42:27 -0300

    fix: :bug: product module complete crud

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ModelNotFoundExceptionFilter } from './exceptionFilters/modelNotFound.exceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new ModelNotFoundExceptionFilter())
   await app.listen(3000);
 }
 bootstrap();
